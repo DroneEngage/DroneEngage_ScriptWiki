17ba930f6aa8ba7b08a2d121516a6948b962ae73
HefnySco <mohammad.hefny@gmail.com>
2025-10-09 11:07:31 +0300

    fix wifi windows issue

--- rpi_image_scripts/bookworm/wifi_start_ap.sh
+++ rpi_image_scripts/bookworm/wifi_start_ap.sh
@@ -1,22 +1,24 @@
+#!/bin/bash
+
+echo "Network may disconnect - wait for 2 min and check for AccessPoint in Wifi List."
+# Stop and disable dnsmasq and hostapd services to avoid conflicts
 sudo systemctl stop dnsmasq
 sudo systemctl disable dnsmasq
 sudo systemctl stop hostapd
 sudo systemctl disable hostapd
-# sudo apt remove dnsmasq hostapd # Only if you are sure you don't need them for other purposes
 
-# sudo systemctl status NetworkManager
+# Delete existing hotspot connection if it exists
+sudo nmcli con delete hotspot
 
-sudo nmcli con delete hotspot # Remove if it exists to start fresh
+# Create a new Wi-Fi AP connection
 sudo nmcli con add type wifi ifname wlan0 con-name hotspot ssid "DE_ADMIN" autoconnect yes
 sudo nmcli con modify hotspot wifi-sec.key-mgmt wpa-psk
 sudo nmcli con modify hotspot wifi-sec.psk "droneengage"
-sudo nmcli con modify hotspot 802-11-wireless.mode ap 802-11-wireless.band bg ipv4.method shared
+sudo nmcli con modify hotspot 802-11-wireless.mode ap 802-11-wireless.band bg
+sudo nmcli con modify hotspot wifi-sec.proto rsn  # Explicitly use WPA2 (RSN)
+sudo nmcli con modify hotspot wifi-sec.pairwise ccmp  # Use CCMP (AES) encryption
+sudo nmcli con modify hotspot wifi-sec.group ccmp
 sudo nmcli con modify hotspot ipv4.method shared ipv4.addresses 192.169.9.1/24
+
+# Bring up the hotspot
 sudo nmcli con up hotspot
-
-#sudo nmcli con show hotspot
-#sudo nmcli device wifi list # See if your hotspot is broadcast
-#sudo nmcli device show wlan0 # Check IP, etc.
-#journalctl -u NetworkManager -f # Watch logs for errors
-
-

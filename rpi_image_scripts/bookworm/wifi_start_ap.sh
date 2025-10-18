a1b0c7992ccf1fe8f2ebc5d795c126f0c23cf2c8
HefnySco <mohammad.hefny@gmail.com>
2025-10-18 09:06:48 +0300

    better logic AP script

--- rpi_image_scripts/bookworm/wifi_start_ap.sh
+++ rpi_image_scripts/bookworm/wifi_start_ap.sh
@@ -2,10 +2,12 @@
 
 echo "Network may disconnect - wait for 2 min and check for AccessPoint in Wifi List."
 # Stop and disable dnsmasq and hostapd services to avoid conflicts
-sudo systemctl stop dnsmasq
-sudo systemctl disable dnsmasq
-sudo systemctl stop hostapd
-sudo systemctl disable hostapd
+# Use '|| true' and '2>/dev/null' to prevent errors if services don't exist
+sudo systemctl stop dnsmasq 2>/dev/null || true
+sudo systemctl disable dnsmasq 2>/dev/null || true
+sudo systemctl stop hostapd 2>/dev/null || true
+sudo systemctl disable hostapd 2>/dev/null || true
+
 
 # Delete existing hotspot connection if it exists
 sudo nmcli con delete hotspot
@@ -19,6 +21,7 @@ sudo nmcli con modify hotspot wifi-sec.proto rsn  # Explicitly use WPA2 (RSN)
 sudo nmcli con modify hotspot wifi-sec.pairwise ccmp  # Use CCMP (AES) encryption
 sudo nmcli con modify hotspot wifi-sec.group ccmp
 sudo nmcli con modify hotspot ipv4.method shared ipv4.addresses 192.169.9.1/24
+sudo nmcli con modify hotspot ipv6.method ignore
 sudo nmcli con modify hotspot 802-11-wireless-security.wps disabled
 
 # Bring up the hotspot

2728782baf208ea18cc0752455f69486ac5a92cd
HefnySco <mohammad.hefny@gmail.com>
2025-10-25 12:09:21 +0300

    fix screipt thermal and add helper for oem release

---
+++ rpi_image_scripts/bookworm/hlp_reset_oem.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+/home/pi/scripts/c_helpers/updateConfig ENTER_ACCOUNT ENTER_PASSWORD /home/pi/drone_engage/de_comm/de_comm.config.module.json /home/pi/simulator/sim_de_mavlink_instances/de_comm.1.config.module.json /home/pi/simulator/sim_de_mavlink_instances/de_comm.2.config.module.json
+
+/home/pi/scripts/wifi_start_ap.sh
+
+/home/pi/scripts/sh_clean_logs.sh
+
+
--- rpi_image_scripts/bookworm/updates/sh_update_thermal_lib.sh
+++ rpi_image_scripts/bookworm/updates/sh_update_thermal_lib.sh
@@ -8,7 +8,9 @@ BLUE='\033[34m'
 NC='\033[0m' # No Color (Reset)
 
 echo -e "${BLUE}Updating Thermal Camera Lib...${NC}"
-source ./senxor_venv/bin/activate
+rm -rf /home/pi/senxor_venv
+python3 -m venv /home/pi/senxor_venv
+source /home/pi/senxor_venv/bin/activate
 pip install git+https://github.com/HefnySco/pysenxor-lite.git@pr_drone_engage_thermal_camera
 deactivate
 

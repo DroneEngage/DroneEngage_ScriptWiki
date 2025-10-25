6981f123f0280a86a26d4b5a57ea9c529243bdf0
HefnySco <mohammad.hefny@gmail.com>
2025-10-25 08:59:14 +0300

    update script for thermal_camera

---
+++ rpi_image_scripts/bookworm/updates/sh_update_thermal_lib.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ANSI Color Definitions
+RED='\033[31m'
+GREEN='\033[32m'
+YELLOW='\033[33m'
+BLUE='\033[34m'
+NC='\033[0m' # No Color (Reset)
+
+echo -e "${BLUE}Updating Thermal Camera Lib...${NC}"
+source ./senxor_venv/bin/activate
+pip install git+https://github.com/HefnySco/pysenxor-lite.git@pr_drone_engage_thermal_camera
+deactivate
+
